0dd3b0aa80d5495975ccc6a25f42ebba34a5821c
Martinimsa <marti_soriano@outlook.es>
2021-03-29 14:14:05 +0200

    seguritat

--- html/cat/temes-cat/s/ex/1/1.js
+++ html/cat/temes-cat/s/ex/1/1.js
@@ -1,21 +1,21 @@
 var punts = 10;
 function call(){
-    enviar(1, "l1", "bessó");
-    enviar(2, "l2", "fracassar");
-    enviar(3, "l3", "classe");
-    enviar(4, "l4", "interessar");
-    enviar(5, "l5", "monosilàbic");
-    enviar(6, "l6", "pessar");
-    enviar(7, "l7", "necessari");
-    enviar(8, "l8", "pèsols");
-    enviar(9, "l9", "casa");
-    enviar(10, "l10", "absurda");
-    enviar(11, "l11", "assignar");
-    enviar(12, "l12", "absolut");
-    enviar(13, "l13", "absència");
-    enviar(14, "l14", "assentir");
-    enviar(15, "l15", "bressol");
-    enviar(16, "l16", "bescanvi");
+    enviar(1, "l1", unescape("%62%65%73%73%F3"));
+    enviar(2, "l2", unescape("%66%72%61%63%61%73%73%61%72"));
+    enviar(3, "l3", unescape("%63%6C%61%73%73%65"));
+    enviar(4, "l4", unescape("%69%6E%74%65%72%65%73%73%61%72"));
+    enviar(5, "l5", unescape("%6D%6F%6E%6F%73%69%6C%E0%62%69%63"));
+    enviar(6, "l6", unescape("%70%65%73%73%61%72"));
+    enviar(7, "l7", unescape("%6E%65%63%65%73%73%61%72%69"));
+    enviar(8, "l8", unescape("%70%E8%73%6F%6C%73"));
+    enviar(9, "l9", unescape("%63%61%73%61"));
+    enviar(10, "l10", unescape("%61%62%73%75%72%64%61"));
+    enviar(11, "l11", unescape("%61%73%73%69%67%6E%61%72"));
+    enviar(12, "l12", unescape("%61%62%73%6F%6C%75%74"));
+    enviar(13, "l13", unescape("%61%62%73%E8%6E%63%69%61"));
+    enviar(14, "l14", unescape("%61%73%73%65%6E%74%69%72"));
+    enviar(15, "l15", unescape("%62%72%65%73%73%6F%6C"));
+    enviar(16, "l16", unescape("%62%65%73%63%61%6E%76%69"));
     alert("Has tret un "+punts.toString());
     punts=10;
 }
--- html/cat/temes-cat/s/ex/2/2.js
+++ html/cat/temes-cat/s/ex/2/2.js
@@ -1,21 +1,21 @@
 var punts = 10;
 function call(){
-    enviar(1, "l1", "forçar");
-    enviar(2, "l2", "camisa");
-    enviar(3, "l3", "endinsa");
-    enviar(4, "l4", "façana");
-    enviar(5, "l5", "terrassa");
-    enviar(6, "l6", "empassar");
-    enviar(7, "l7", "espessir");
-    enviar(8, "l8", "enrossir");
-    enviar(9, "l9", "França");
-    enviar(10, "l10", "enfonsen");
-    enviar(11, "l11", "embussada");
-    enviar(12, "l12", "mudança");
-    enviar(13, "l13", "ésser");
-    enviar(14, "l14", "travessa");
-    enviar(15, "l15", "calçotada");
-    enviar(16, "l16", "falç");
+    enviar(1, "l1", unescape("%66%6F%72%E7%61%72"));
+    enviar(2, "l2", unescape("%63%61%6D%69%73%61"));
+    enviar(3, "l3", unescape("%65%6E%64%69%6E%73%61"));
+    enviar(4, "l4", unescape("%66%61%E7%61%6E%61"));
+    enviar(5, "l5", unescape("%74%65%72%72%61%73%73%61"));
+    enviar(6, "l6", unescape("%65%6D%70%61%73%73%61%72"));
+    enviar(7, "l7", unescape("%65%73%70%65%73%73%69%72"));
+    enviar(8, "l8", unescape("%65%6E%72%6F%73%73%69%72"));
+    enviar(9, "l9", unescape("%46%72%61%6E%E7%61"));
+    enviar(10, "l10", unescape("%65%6E%66%6F%6E%73%65%6E"));
+    enviar(11, "l11", unescape("%65%6D%62%75%73%73%61%64%61"));
+    enviar(12, "l12", unescape("%6D%75%64%61%6E%E7%61"));
+    enviar(13, "l13", unescape("%E9%73%73%65%72"));
+    enviar(14, "l14", unescape("%74%72%61%76%65%73%73%61"));
+    enviar(15, "l15", unescape("%63%61%6C%E7%6F%74%61%64%61"));
+    enviar(16, "l16", unescape("%66%61%6C%E7"));
     alert("Has tret un "+punts.toString());
     punts=10;
 }
